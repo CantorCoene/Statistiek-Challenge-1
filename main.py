48fc8cbcaef1bfd2f9df671b1d54393330f5c9b1
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2015-11-04 12:22:01 +0100

    Een bugje weggehaald in de code: Had een foute index gebruikt in een lijst

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Nodige imports
 import statistics
 
@@ -9,28 +10,27 @@ data_file = open('testdata.txt', 'r')
 data_string = data_file.read()
 data_list = data_string.splitlines()
 
+
 # Om al de waarden voor y_s te krijgen maken we een for lus die over alle elementen gaat van data_list
 # Alle waarden voor y_s slagen we op in een lijst.
 y_s_list = []
 for lines in data_list:
-    try:
-        # omdat onze line variabele van de volgende vorm is:
-        # "n1\tp1\tn2\tp2"
-        # vinden we dat p1 = line[2] en p2 = line[6]
-        y2 = float(lines[6])*0.1
-        y1 = float(lines[2])*0.1
+	
+    # omdat onze line variabele van de volgende vorm is:
+    # "n1\tp1\tn2\tp2"
+    # vinden we dat p1 = line[2] en p2 = line[6]
+    
+    y2 = int(lines[7])*0.1
+    y1 = int(lines[2])*0.1
 
-        X2 = 10.7 # Waarde voor groep 7
-        X1 = 4.4
+    X2 = 10.7 # Waarde voor groep 7
+    X1 = 4.4
 
-        rico = (y2 - y1)/(X2 - X1)
+    rico = (y2 - y1)/(X2 - X1)
 
-        y_s = y1 - (X1*rico)
+    y_s = y1 - (X1*rico)
 
-        y_s_list.append(y_s)
-    except:
-        print("Er liep iets fout bij de berekening van y_s in de for lus.")
-        break
+    y_s_list.append(y_s)
 
 # We berekenen de standaard afwijking met de statistics package uit python 3.4.
 y_s_std = statistics.stdev(y_s_list)
